47563cb3f12f40bb6eaa6e217d86e0224790680b
Adrian Moser <trekking09@gmail.com>
2017-01-17 16:16:51 +0100

    updated

--- app/gui-model/guimodel.ts
+++ app/gui-model/guimodel.ts
@@ -2,7 +2,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Path Example",
+            "title": "Peters Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
